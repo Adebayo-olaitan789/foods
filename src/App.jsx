e784333e701696d5d400b553ac75fc4b1c43d1b2
Adebayo <anurudeen511@gmail.com>
2024-10-06 22:20:15 -1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Search from "./component/Search";
+import FoodList from "./component/FoodList";
+import Nav from "./component/Nav";
+import "./App.css";
+import Container from "./component/Container";
+import InnerContainer from "./component/InnerContainer";
+import FoodDetails from "./component/FoodDetails";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("658615");
+  return (
+    <div>
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
